2d8461a68f82b1dcfea5e29525c60de9ff2d6e34
hangyu cho <chg3376@gmail.com>
2020-07-12 23:29:00 +0900

    making home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,15 +12,54 @@ class _HomeScreenState extends State<HomeScreen> {
       'title' : '【予約制：akippa】北斗市追分1丁目3-39駐車場',
       'address' : '北海道北斗市追分1丁目3-39',
       'parkingAreaType' : '時間貸',
-      'closeInfo' : 'なし（詳細は備考のURLのリンク先をご確認ください）',
+      'closeInfo' : 'なし\n（詳細は備考のURLの\nリンク先をご確認ください）',
       'serviceTime' : '8:00～18:00',
       'price' : '当日最大350円',
       'parkingQtyPerOnetime' : '1台',
-      'parkingInfoDetail' : '排気量による制限はありません。詳細はリンク先をご確認ください。',
+      'parkingInfoDetail' : '排気量による制限はありません。\n詳細はリンク先をご確認ください。',
       'remark' : '▼ご利用の際は駐車場予約サービス「akippa」のサイトよりご予約ください。https://www.akippa.com/parking/268217bf16184b606e3d2a4fe5901e27?utm_source=jmpsa&utm_medium=referral&utm_campaign=jmpsa',
       'managementCompany' : 'akippa株式会社',
       'updateDate' : DateTime.now()
-    })
+    }),
+    Parking.fromMap({
+      'title' : '【予約制：akippa】北斗市追分1丁目3-39駐車場',
+      'address' : '北海道北斗市追分1丁目3-39',
+      'parkingAreaType' : '時間貸',
+      'closeInfo' : 'なし\n（詳細は備考のURLの\nリンク先をご確認ください）',
+      'serviceTime' : '8:00～18:00',
+      'price' : '当日最大350円',
+      'parkingQtyPerOnetime' : '1台',
+      'parkingInfoDetail' : '排気量による制限はありません。\n詳細はリンク先をご確認ください。',
+      'remark' : '▼ご利用の際は駐車場予約サービス「akippa」のサイトよりご予約ください。https://www.akippa.com/parking/268217bf16184b606e3d2a4fe5901e27?utm_source=jmpsa&utm_medium=referral&utm_campaign=jmpsa',
+      'managementCompany' : 'akippa株式会社',
+      'updateDate' : DateTime.now()
+    }),
+    Parking.fromMap({
+      'title' : '【予約制：akippa】北斗市追分1丁目3-39駐車場',
+      'address' : '北海道北斗市追分1丁目3-39',
+      'parkingAreaType' : '時間貸',
+      'closeInfo' : 'なし\n（詳細は備考のURLの\nリンク先をご確認ください）',
+      'serviceTime' : '8:00～18:00',
+      'price' : '当日最大350円',
+      'parkingQtyPerOnetime' : '1台',
+      'parkingInfoDetail' : '排気量による制限はありません。\n詳細はリンク先をご確認ください。',
+      'remark' : '▼ご利用の際は駐車場予約サービス「akippa」のサイトよりご予約ください。https://www.akippa.com/parking/268217bf16184b606e3d2a4fe5901e27?utm_source=jmpsa&utm_medium=referral&utm_campaign=jmpsa',
+      'managementCompany' : 'akippa株式会社',
+      'updateDate' : DateTime.now()
+    }),
+    Parking.fromMap({
+      'title' : '【予約制：akippa】北斗市追分1丁目3-39駐車場',
+      'address' : '北海道北斗市追分1丁目3-39',
+      'parkingAreaType' : '時間貸',
+      'closeInfo' : 'なし\n（詳細は備考のURLの\nリンク先をご確認ください）',
+      'serviceTime' : '8:00～18:00',
+      'price' : '当日最大350円',
+      'parkingQtyPerOnetime' : '1台',
+      'parkingInfoDetail' : '排気量による制限はありません。\n詳細はリンク先をご確認ください。',
+      'remark' : '▼ご利用の際は駐車場予約サービス「akippa」のサイトよりご予約ください。https://www.akippa.com/parking/268217bf16184b606e3d2a4fe5901e27?utm_source=jmpsa&utm_medium=referral&utm_campaign=jmpsa',
+      'managementCompany' : 'akippa株式会社',
+      'updateDate' : DateTime.now()
+    }),
   ];
   @override
   void initState() {
--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -1,6 +1,7 @@
 import 'package:bike_parking_info/model/model_parking.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Parking> parkings;
@@ -80,10 +81,311 @@ class _CarouselImageState extends State<CarouselImage> {
             )
           ),
           Container(
-            child: Text(_currentTitle),
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentTitle,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        padding: EdgeInsets.all(4),
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(padding: EdgeInsets.all(3)),
+                            Text(
+                              '駐車場形態',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      parkingAreaType[_currentPage] == "時間貸"
+                      ? IconButton(
+                        icon: Icon( Icons.timer ), 
+                        
+                        onPressed: () {},
+                      ) 
+                      : IconButton(
+                        icon: Icon( Icons.calendar_today), 
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        padding: EdgeInsets.all(4),
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(3)
+                            ),
+                            Text(
+                              '利用可能時間',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(13.0),
+                          child: Text(
+                            serviceTime[_currentPage]
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        padding: EdgeInsets.all(4),
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(padding: EdgeInsets.all(3)),
+                            Text(
+                              '料金（時間貸）',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(13.0),
+                          child: Text(
+                            price[_currentPage]
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        padding: EdgeInsets.all(4),
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(padding: EdgeInsets.all(3)),
+                            Text(
+                              '定休日',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            closeInfo[_currentPage]
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: [
+                      FlatButton(
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(3)
+                            ),
+                            Text(
+                              '収容台数',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            parkingQtyPerOnetime[_currentPage]
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(padding: EdgeInsets.all(3)),
+                            Text(
+                              '管理会社',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            managementCompany[_currentPage]
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      FlatButton(
+                        color: Colors.white,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.announcement,
+                              color: Colors.black,
+                            ),
+                            Padding(padding: EdgeInsets.all(3)),
+                            Text(
+                              '最終更新日',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            DateFormat('yy/MM/dd').format(updateDate[_currentPage]), 
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: makeIndicator(title, _currentPage),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+  List<Widget> makeIndicator(List list, int _currentPage) {
+    List<Widget> results = [];
+    for (var i = 0; i < list.length; i++) {
+      results.add(Container(
+        width: 8,
+        height: 8,
+        margin: EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 2.0
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: _currentPage == i
+                ? Color.fromRGBO(255, 255, 255, 0.9)
+                : Color.fromRGBO(255, 255, 255, 0.4)
+        )
+      ));
+    }
+    return results;
+  }
